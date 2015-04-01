6e0fb4f036bab2024232373bee59a7db24d88424
Xiaodong Jiang <jxdwinter@gmail.com>
2015-04-01 09:56:24 +0800

    init commit

---
+++ TouchTracker/TouchTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchTracker
+//
+//  Created by Xiaodong Jiang on 4/1/15.
+//  Copyright (c) 2015 Xiaodong Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchTracker/TouchTracker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TouchTracker
+//
+//  Created by Xiaodong Jiang on 4/1/15.
+//  Copyright (c) 2015 Xiaodong Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
